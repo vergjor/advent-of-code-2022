0b8e7879f81e46638b148f4c330c613e24980186
Veronika Gjoreva <veronika@kdsol.net>
2022-12-04 23:46:00 +0100

    Solution added for Day 1: Calorie Counting - Part 2

--- day-01/solution.go
+++ day-01/solution.go
@@ -7,10 +7,59 @@ import (
 	"os"
 )
 
+func isCalorieStackPopulated(calorieStack [3]int) bool {
+	return calorieStack[0] != 0 && calorieStack[1] != 0 && calorieStack[2] != 0
+}
+
+func sortCalories(calorieStack *[3]int) {
+	calories := *calorieStack
+	
+	for index := 2; index >= 0; index-- {
+		for i, value := range calories[:index] {
+			if value < calories[index] {
+				tmp := calories[index]
+				calories[index] = value
+				calories[i] = tmp
+			}
+		}
+	}
+
+	*calorieStack = calories
+}
+
+func populateCalorieStack(calorieCount int, calorieStack *[3]int) {
+	calories := *calorieStack
+	
+	for index, value := range calories {
+		if value == 0 && (calorieStack[0] != calorieCount || calorieStack[1] != calorieCount || calorieStack[2] != calorieCount) {
+			calories[index] = calorieCount
+			sortCalories(&calories)
+			*calorieStack = calories
+			break;
+		}
+	}
+}
+
+func updateTopThreeElvenCalorieCount(calorieCount int, calorieStack *[3]int) {
+	calories := *calorieStack
+
+	for index := 2; index >= 0; index-- {
+		if calories[index] < calorieCount {
+			calories[index] = calorieCount
+			break;
+		}
+	}
+
+	sortCalories(&calories)
+	*calorieStack = calories
+}
+
 func main() {
-	var highestCalorieCount, currentElfCalorieCount int = 0, 0
+	var topThreeStackedElves [3]int
+	currentElfCalorieCount, totalCaloriesInStack := 0, 0
 
 	readFile, _  := os.Open("data.txt")
+	defer readFile.Close()
 	scanner := bufio.NewScanner(readFile)
 	scanner.Split(bufio.ScanLines)
 
@@ -18,8 +67,10 @@ func main() {
 		fileLine := fmt.Sprintf("%v", scanner.Text())
 
 		if len(fileLine) == 0 {
-			if highestCalorieCount < currentElfCalorieCount {
-				highestCalorieCount = currentElfCalorieCount
+			if isCalorieStackPopulated(topThreeStackedElves) {
+				updateTopThreeElvenCalorieCount(currentElfCalorieCount, &topThreeStackedElves)
+			} else {
+				populateCalorieStack(currentElfCalorieCount, &topThreeStackedElves)
 			}
 			currentElfCalorieCount = 0
 			continue;
@@ -29,6 +80,9 @@ func main() {
 		currentElfCalorieCount += currentFoodCalories
 	}
 
-	fmt.Println(highestCalorieCount)
-	readFile.Close()
+	for _, totalCalories := range topThreeStackedElves {
+		totalCaloriesInStack += totalCalories
+	}
+
+	fmt.Println(totalCaloriesInStack)
 }
